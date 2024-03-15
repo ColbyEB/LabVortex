90f49738e67c22de05b18afc695e668000c1588c
77209721 <77209721+ColbyEB@users.noreply.github.com>
2024-03-15 19:00:20 -0400

    fixes day 1000 million

--- turntablecontroller.py
+++ turntablecontroller.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 
-pwm_high_pin = 6 # not right pin YET
-pwm_low_pin = 9 # ALSO NOT RIGHT PIN PLEASE CONFIGURE
+pwm_high_pin = 19 # not right pin YET
+pwm_low_pin = 26 # ALSO NOT RIGHT PIN PLEASE CONFIGURE
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pwm_high_pin, GPIO.OUT)
@@ -9,17 +9,18 @@ GPIO.setup(pwm_low_pin, GPIO.OUT)
 
 pwm_high = GPIO.PWM(pwm_high_pin, 100)  # 344 Hz frequency
 pwm_low = GPIO.PWM(pwm_low_pin, 100)    # 1000 Hz frequency
+pwm_high.start(0)
+pwm_low.start(0)
 
 def pwmStartFwd():
-    pwm_high.stop()
-    pwm_low.stop()
-    pwm_high.start(6)  # Start PWM with 6% duty cycle
-    pwm_low.start(0)   # Start PWM with 0% duty cycle
+    while True:
+        pwm_high.ChangeDutyCycle(15)  # Start PWM with 6% duty cycle
+        pwm_low.ChangeDutyCycle(0)   # Start PWM with 0% duty cycle
 
 def pwmStartReverse():
-    pwm_high.stop()
-    pwm_low.stop()
-    pwm_high.start(0)  # Start PWM with 6% duty cycle
-    pwm_low.start(6)   # Start PWM with 0% duty cycle
+    while True:
+        pwm_high.ChangeDutyCycle(13)  # Start PWM with 6% duty cycle
+        pwm_low.ChangeDutyCycle(0)   # Start PWM with 0% duty cycle
 
-
+pwmStartReverse()
+print("donezo")
